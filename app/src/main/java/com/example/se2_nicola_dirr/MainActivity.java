cd16806714db3685085ee97321fd6e300908f4fc
nicola <nidirr@edu.aau.at>
2021-03-11 17:35:32 +0100

    Tcp connection and task 2 created

--- app/src/main/java/com/example/se2_nicola_dirr/MainActivity.java
+++ app/src/main/java/com/example/se2_nicola_dirr/MainActivity.java
@@ -8,11 +8,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 public class MainActivity extends AppCompatActivity {
 
     private Button button1;
-    private TextView textView4;
+    private TextView textView1;
     private EditText textNumber1;
+    private Button button2;
+    private TextView textView2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +28,61 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button1 = (Button) findViewById(R.id.button1);
-        textView4 = (TextView) findViewById(R.id.textView4);
+        textView1 = (TextView) findViewById(R.id.textView1);
         textNumber1 = (EditText) findViewById(R.id.textNumber1);
+        button2 = (Button) findViewById(R.id.button2);
+        textView2 = (TextView) findViewById(R.id.textView2);
+
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // OnClick Methode legt fest was passiert, wenn man auf den Button Klickt -> Bei uns wird Matrikelnr an den Server geschickt und das ergebnis zurück via TCP
-                textView4.setText("Ihre Matrikelnummer: ");
+
+                String st = textNumber1.getText().toString();
+                StringBuilder studentNumber = new StringBuilder(st);
+                int p = 97;
+
+                for (int i = 1; i < st.length(); i++) {
+                    if (i % 2 != 0) {
+                        studentNumber.setCharAt(i, (char) p);
+                        p++;
+
+                    }
+                }
+                textView1.setText(studentNumber);
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                run();
 
             }
         });
 
+    }
+
+    public void run() {
+        ExampleThread thread = new ExampleThread();
+        thread.start();
+
+    }
+
+    class ExampleThread extends Thread {
+        @Override
+        public void run() {
+            try {
+                String textServer = textNumber1.getText().toString();
+                Socket clientTCP = new Socket("se2-isys.aau.at", 53212);
+                DataOutputStream outToServer = new DataOutputStream(clientTCP.getOutputStream());
+                BufferedReader fromServer = new BufferedReader(new InputStreamReader(clientTCP.getInputStream()));
+                outToServer.writeBytes(textServer + '\n');
+                textView2.setText(fromServer.readLine());
+                clientTCP.close();
+            } catch (Exception  NetworkError) {
+                System.out.println("Error");
+            }
+        }
     }
 }
